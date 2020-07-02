13f743fbd009b1d78e9fe2bf46ed72e563379a34
Sahithi <67451120+sahithi2316@users.noreply.github.com>
2020-07-02 20:09:11 +0530

    Add files via upload

---
+++ Assignment 12_reverse_tuple.py
@@ -0,0 +1,6 @@
+a=("python")
+b=reversed(a)
+print(tuple(b))
+a=(2,4,6,8,10)
+b=reversed(a)
+print(tuple(b))
---
+++ Assignment 12_tuple_to_string.py
@@ -0,0 +1,6 @@
+def convertTuple(tup):
+    str=' '.join(tup)
+    return str
+tuple=('p','y','t','h','o','n')
+str=convertTuple(tuple)
+print(str)
