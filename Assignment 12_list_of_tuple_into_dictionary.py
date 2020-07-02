8e1a347da5004bb6a88c65c393efc7b8949638a5
Sahithi <67451120+sahithi2316@users.noreply.github.com>
2020-07-02 22:58:10 +0530

    Add files via upload

---
+++ Assignment 12_list_of_tuple_into_dictionary.py
@@ -0,0 +1,6 @@
+def listtodict(X,di):
+    di=dict(A)
+    return di
+A=[("Sahithi",30),("Lakshmi",32),("Teja",20),("Rohini",34)]
+di={}
+print("the dictionary is::>",listtodict(A,di))
---
+++ Assignment 12_list_to_tuple.py
@@ -0,0 +1,4 @@
+list=[2,4,6,8,10]
+print(list)
+tuplex=tuple(list)
+print(tuplex)
